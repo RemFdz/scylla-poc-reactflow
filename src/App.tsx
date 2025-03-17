8340a7e503b98aead42b7a5d7554da718aae4ea6
RemFdz <remi.fernandez@epitech.eu>
2025-03-17 14:18:29 +0100

    feat: default react flow and ws connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {ReactFlow} from "@xyflow/react";
+import '@xyflow/react/dist/style.css';
+import {RefObject, useEffect, useRef, useState} from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const wsRef: RefObject<null | WebSocket> = useRef(null);
+    const [isConnected, setConnected] = useState(false);
+
+
+    useEffect(() => {
+        if (wsRef.current)
+            return;
+        const ws = new WebSocket('ws://localhost:8080/ws');
+        wsRef.current = ws;
+        ws.onopen = () => {
+            console.log('WS connected to the server');
+            setConnected(true);
+        }
+        ws.onerror = (e) => console.error('WS error: ', e);
+        ws.onclose = () => console.log('WS connection closed');
+
+        return () => {
+            if (wsRef.current && isConnected) {
+                wsRef.current.close()
+                wsRef.current = null;
+            }
+        }
+    });
+
+    const initialNodes = [
+        { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+        { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+    ];
+    const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{display: 'flex', alignItems: 'center', flexDirection: 'column', width: '100%', height: '100%'}}>
+          {isConnected ? <p style={{color: 'green'}}>Connected</p>: <p style={{color: 'red'}}>Disconnected</p>}
+          <div style={{width: '75%', height: '75%', border: 'solid 1px black'}}>
+                  <ReactFlow nodes={initialNodes} edges={initialEdges}/>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
